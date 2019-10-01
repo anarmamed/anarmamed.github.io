37c70810fbaeadc31a4a80edf911b8799141d62f
anarmamed <53862345+anarmamed@users.noreply.github.com>
2019-10-01 12:30:17 +0300

    mailer final

--- shtukaturka/mailer/smart.php
+++ shtukaturka/mailer/smart.php
@@ -1,7 +1,7 @@
 <?php 
 
-$name = $_POST['user_name'];
-$phone = $_POST['user_phone'];
+$name = $_POST['input-name'];
+$phone = $_POST['phone'];
 
 require_once('phpmailer/PHPMailerAutoload.php');
 $mail = new PHPMailer;
@@ -10,15 +10,15 @@ $mail->CharSet = 'utf-8';
 //$mail->SMTPDebug = 3;                               // Enable verbose debug output
 
 $mail->isSMTP();                                      // Set mailer to use SMTP
-$mail->Host = 'smtp.mail.ru';  // Specify main and backup SMTP servers
+$mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = 'mail@mail.ru';                 // Наш логин
-$mail->Password = '032ujfw9';                           // Наш пароль от ящика
+$mail->Username = 'anarka2@gmail.com';                 // Наш логин
+$mail->Password = '10980511Gmail';                           // Наш пароль от ящика
 $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 465;                                    // TCP port to connect to
  
-$mail->setFrom('mail@mail.ru', 'John Doe');   // От кого письмо 
-$mail->addAddress('mail@yandex.ru');     // Add a recipient
+$mail->setFrom('anarka2@gmail.com', 'Anar');   // От кого письмо 
+$mail->addAddress('anarmamedramis@gmail.com');     // Add a recipient
 //$mail->addAddress('ellen@example.com');               // Name is optional
 //$mail->addReplyTo('info@example.com', 'Information');
 //$mail->addCC('cc@example.com');
