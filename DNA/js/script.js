3f67ce2e0fe1781faf004558604a5b8178aefebf
Anar Mamedov <53862345+anarmamed@users.noreply.github.com>
2019-10-24 17:43:11 +0300

    Залил сайт DNA

---
+++ DNA/js/script.js
@@ -0,0 +1,75 @@
+$('.btn-food').click(function(){
+  $(this).toggleClass('btn-food-active');
+  });
+
+
+  $('.ch-1').click(function(){
+    $(this).toggleClass('actvive-chek_2');
+    $('#span_ch_1').toggleClass('actvive-chek_span');
+    $('div.chek-div-2').removeClass('actvive-chek_2');
+    $('.a_sp_2').removeClass('actvive-chek_span');
+    $('div.chek-div-3').removeClass('actvive-chek_2');
+    $('.a_sp_3').removeClass('actvive-chek_span');
+    $('div.chek-div-4').removeClass('actvive-chek_2');
+    $('.a_sp_4').removeClass('actvive-chek_span');
+    $('div.chek-div-5').removeClass('actvive-chek_2');
+    $('.a_sp_5').removeClass('actvive-chek_span');
+    
+    })
+    
+    
+    $('.ch-2').click(function(){
+    $(this).toggleClass('actvive-chek_2');
+    $('#span_ch_2').toggleClass('actvive-chek_span');
+    $('div.chek-div-1').removeClass('actvive-chek_2');
+    $('.a_sp_1').removeClass('actvive-chek_span');
+    $('div.chek-div-3').removeClass('actvive-chek_2');
+    $('.a_sp_3').removeClass('actvive-chek_span');
+    $('div.chek-div-4').removeClass('actvive-chek_2');
+    $('.a_sp_4').removeClass('actvive-chek_span');
+    $('div.chek-div-5').removeClass('actvive-chek_2');
+    $('.a_sp_5').removeClass('actvive-chek_span');
+    });
+    
+    
+    $('.ch-3').click(function(){
+    $(this).toggleClass('actvive-chek_2');
+    $('#span_ch_3').toggleClass('actvive-chek_span');
+    $('div.chek-div-1').removeClass('actvive-chek_2');
+    $('.a_sp_1').removeClass('actvive-chek_span');
+    $('div.chek-div-2').removeClass('actvive-chek_2');
+    $('.a_sp_2').removeClass('actvive-chek_span');
+    $('div.chek-div-4').removeClass('actvive-chek_2');
+    $('.a_sp_4').removeClass('actvive-chek_span');
+    $('div.chek-div-5').removeClass('actvive-chek_2');
+    $('.a_sp_5').removeClass('actvive-chek_span');
+    });
+    
+    
+    $('.ch-4').click(function(){
+    $(this).toggleClass('actvive-chek_2');
+    $('#span_ch_4').toggleClass('actvive-chek_span');
+    $('div.chek-div-1').removeClass('actvive-chek_2');
+    $('.a_sp_1').removeClass('actvive-chek_span');
+    $('div.chek-div-2').removeClass('actvive-chek_2');
+    $('.a_sp_2').removeClass('actvive-chek_span');
+    $('div.chek-div-3').removeClass('actvive-chek_2');
+    $('.a_sp_3').removeClass('actvive-chek_span');
+    $('div.chek-div-5').removeClass('actvive-chek_2');
+    $('.a_sp_5').removeClass('actvive-chek_span');
+    });
+    
+    $('.ch-5').click(function(){
+    $(this).toggleClass('actvive-chek_2');
+    $('#span_ch_5').toggleClass('actvive-chek_span');
+    
+    $('div.chek-div-1').removeClass('actvive-chek_2');
+    $('.a_sp_1').removeClass('actvive-chek_span');
+    $('div.chek-div-2').removeClass('actvive-chek_2');
+    $('.a_sp_2').removeClass('actvive-chek_span');
+    $('div.chek-div-3').removeClass('actvive-chek_2');
+    $('.a_sp_3').removeClass('actvive-chek_span');
+    $('div.chek-div-4').removeClass('actvive-chek_2');
+    $('.a_sp_4').removeClass('actvive-chek_span');
+    });
+    
