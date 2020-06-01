638912c40dd8db11196972efb7143480bae8f84e
Anar Mamedov <53862345+anarmamed@users.noreply.github.com>
2020-06-01 16:14:51 +0300

    Add files via upload

---
+++ GoSerf/js/main.js
@@ -0,0 +1,47 @@
+$(function(){
+
+  $('.header__slider').slick({
+    infinite: true,
+    fade: true,
+    prevArrow: '<img class="slick-arrow__left"  src="img/arrow-left.svg">',
+    nextArrow: '<img class="slick-arrow__right"  src="img/arrow-right.svg">',
+    asNavFor: '.slider-dots'
+  });
+  $('.slider-dots').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    asNavFor: '.header__slider',
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          infinite: true,
+          prevArrow: false,
+          nextArrow: false
+        }
+      },
+      {
+        breakpoint: 840,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          infinite: true,
+          prevArrow: false,
+          nextArrow: false
+        }
+      },
+      {
+        breakpoint: 640,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true,
+          prevArrow: false,
+          nextArrow: false
+        }
+      },
+    ]
+  });
+});
