9bcd8ce88431aa48816ec0262f16d6a6491f6f7c
Anar Mamedov <53862345+anarmamed@users.noreply.github.com>
2020-06-04 11:55:50 +0300

    change second section

--- GoSerf/js/main.js
+++ GoSerf/js/main.js
@@ -5,10 +5,10 @@ $(function(){
     fade: true,
     prevArrow: '<img class="slick-arrow__left"  src="img/arrow-left.svg">',
     nextArrow: '<img class="slick-arrow__right"  src="img/arrow-right.svg">',
-    asNavFor: '.slider-dotshead'
+    asNavFor: '.slider-dots__head'
   });
 
-  $('.slider-dotshead').slick({
+  $('.slider-dots__head').slick({
     slidesToShow: 4,
     slidesToScroll: 1,
     asNavFor: '.header__slider',
@@ -52,10 +52,39 @@ $(function(){
     prevArrow: '<img class="slick-arrow__left"  src="img/arrow-left.svg">',
     nextArrow: '<img class="slick-arrow__right"  src="img/arrow-right.svg">',
     asNavFor: '.slider-map',
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          infinite: true,
+          
+        }
+      },
+      {
+        breakpoint: 840,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          infinite: true,
+         
+        }
+      },
+      {
+        breakpoint: 640,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true,
+         
+        }
+      },
+    ]
   });
 
   $('.slider-map').slick({
-    slidesToShow: 8,
+    slidesToShow: 5,
     slidesToScroll: 1,
     arrows: false,
     asNavFor: '.surf-slider',
