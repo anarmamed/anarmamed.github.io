270d38e0b8f6ce2ebd387dae50d36fb11d2400dd
Anar Mamedov <53862345+anarmamed@users.noreply.github.com>
2020-07-05 19:00:26 +0300

    Add files via upload

--- Repa/js/main.js
+++ Repa/js/main.js
@@ -36,7 +36,56 @@ headerMenu.addEventListener('click', (event) => {
 
 
 
+
+
 $(document).ready(function(){
+
+	$('.offer-slider').slick({
+		infinite: true,
+		slidesToShow: 5,
+		slidesToScroll: 1,
+		arrows: false,
+		responsive: [
+			{
+				breakpoint: 1280,
+				settings: {
+				slidesToShow: 4,
+				slidesToScroll: 1,
+				infinite: true,
+				dots: false,
+				}
+			},
+			{
+				breakpoint: 992,
+				settings: {
+				slidesToShow: 3,
+				slidesToScroll: 1,
+				infinite: true,
+				dots: false,
+				}
+			},
+			{
+				breakpoint: 768,
+				settings: {
+				slidesToShow: 2,
+				slidesToScroll: 1,
+				infinite: true,
+				dots: false,
+				}
+			},
+			{
+				breakpoint: 600,
+				settings: {
+				slidesToShow: 1,
+				slidesToScroll: 1,
+				infinite: true,
+				dots: false,
+				}
+			}
+				
+			]
+	});
+
 	$('.cases-slider').slick({
 		infinite: true,
 		slidesToShow: 2,
@@ -74,6 +123,20 @@ $(document).ready(function(){
 			infinite: true,
 			dots: false,
 			},
+	});
+
+
+
+	
+	$('.popup-gallery').magnificPopup({
+		delegate: 'a',
+		type: 'image',
+		tLoading: 'Loading... #%curr%...',
+		gallery: {
+			enabled: true,
+			navigateByImgClick: true,
+			preload: [0, 1] // Will preload 0 - before current, and 1 after the current image
+		}
 	});
 
 });
